672888c36bd5a56a97626955802d2943f91cb3fd
Luciano <lucho.2012.tandil@gmail.com>
2024-11-08 18:45:44 -0300

    se agrega vista para visualizar atlas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="row">
     <div class="col-7 border">
-      <VisorCmp />
+      <div class="row justify-content-center">
+        <div class="col-auto">
+          <button type="button" class="btn btn-primary" @click="opcion_vista = 'VISOR'">Visor</button>
+          <button type="button" class="btn btn-primary" @click="opcion_vista = 'ATLAS'">Atlas</button>
+        </div>
+      </div>
+      <VisorCmp     v-if="opcion_vista == 'VISOR'"/>
+      <AtlasEditor  v-if="opcion_vista == 'ATLAS'"/>
     </div>
     <div class="col-5 border">
       <BarraLateral />
@@ -11,8 +18,13 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 import BarraLateral from './components/BarraLateral.vue';
 import VisorCmp from './components/VisorCmp.vue';
+import AtlasEditor from './components/AtlasEditor.vue';
+
+const opcion_vista = ref('VISOR')
 </script>
 
 <style scoped>
---
+++ src/components/AtlasEditor.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <img src="http://localhost:5000/assets/atlas1.png" class="img-responsive" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>

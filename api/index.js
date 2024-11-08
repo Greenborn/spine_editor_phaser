32c4477ea9de9faa004ead6d72f43cb649e88e60
Luciano <lucho.2012.tandil@gmail.com>
2024-11-08 02:57:16 -0300

    Se agrega api para manipulación de archivos

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require("body-parser")
+const fs = require("fs")
+dotenv.config();
+
+const app = express();
+const port = process.env.service_port_api;
+
+app.use(bodyParser.json({ limit: '10mb' }));
+
+app.get('/get', (req, res) => {
+    console.log('/get')//, req.body);
+
+    return res.status(200).send({ "item": true });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en puerto ${port}`);
+});

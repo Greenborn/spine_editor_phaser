f29fd575beb49c0b43cd976cf0b9f2daf5da5846
Luciano <lucho.2012.tandil@gmail.com>
2024-11-08 02:51:55 -0300

    se agrega proyecto vue de front

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col-8 border">
+      <VisorCmp />
+    </div>
+  </div>
+  
+</template>
+
+<script setup>
+import VisorCmp from './components/VisorCmp.vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/VisorCmp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <iframe class="visor-iframe" src="http://localhost:8080" frameborder="0"></iframe>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>;
+.visor-iframe{
+  width: 100%;
+  height: 80vh;
+  background: #ddd;
+}
+</style>
